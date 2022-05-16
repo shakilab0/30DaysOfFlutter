f3f40e40eee511f47a256e60236f7f5b5a8fdd17
ShakilAB0 <shakilahmedsb0@gmail.com>
2022-05-16 14:18:53 +0600

    day 1 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("first app"),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Container(
+          child: Text("My new Project Starttoday"),
+        ),
+      ),
+    );
+  }
+}
+
+    
